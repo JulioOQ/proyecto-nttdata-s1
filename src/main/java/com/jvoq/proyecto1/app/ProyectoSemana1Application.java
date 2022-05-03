ce052d2431419da60deaab0fa642aaf9f3d0cf29
Julio Vladimir Ore Quispe <jorequis@emeal.nttdata.com>
2022-05-03 15:45:50 -0500

    subiendo v1

---
+++ src/main/java/com/jvoq/proyecto1/app/ProyectoSemana1Application.java
@@ -0,0 +1,13 @@
+package com.jvoq.proyecto1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSemana1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSemana1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvoq/proyecto1/app/ProyectoSemana1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvoq.proyecto1.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSemana1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
