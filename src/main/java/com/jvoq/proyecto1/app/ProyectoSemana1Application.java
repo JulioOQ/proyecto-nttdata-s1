4547adc8d4acc29e71c416b2e6d7dd2b6c583896
Juan R Cardenas C <cjuanronal@gmail.com>
2022-05-04 16:19:07 -0500

    CRUD cuentas + cambio de DB a cloud

--- src/main/java/com/jvoq/proyecto1/app/ProyectoSemana1Application.java
+++ src/main/java/com/jvoq/proyecto1/app/ProyectoSemana1Application.java
@@ -3,8 +3,6 @@ package com.jvoq.proyecto1.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.jvoq.proyecto1.app.models.entity.Product;
-
 @SpringBootApplication
 public class ProyectoSemana1Application {
 
